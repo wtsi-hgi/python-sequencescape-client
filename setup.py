3f615c4f6fae224be7ef78b208d49e7a7de56683
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-10-28 14:27:29 +0000

    Moved to using `typing` from mypy-lang package to support versions of Python less than 3.5.

--- setup.py
+++ setup.py
@@ -3,24 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="sequencescape-python-client",
 
-    version="0.1.4",
+    version="0.1.5",
 
     author="Colin Nolan",
     author_email="hgi@sanger.ac.uk",
 
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(exclude=["testing"]),
 
     url="https://github.com/wtsi-hgi/sequencescape-python-client",
 
     license="LICENSE",
 
     description="Python client for interfacing with a Sequencescape database.",
-    long_description=open("README").read(),
+    long_description=open("README.md").read(),
 
-    install_requires=[
-        "SQLAlchemy==1.0.9",
-        "mysqlclient==1.3.6"
-    ],
+    install_requires=open("requirements.txt").read().splitlines(),
 
-    test_suite="sequencescape.tests"
+    test_suite="sequencescape.testing"
 )
