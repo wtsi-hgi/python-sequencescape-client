e56006ab9467f59b861bcaa6de5bc322ec896d99
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-11-16 15:04:35 +0000

    Added syntax check for Python example in the README.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Run Python unit tests
 ./scripts/pip-install-requirements.sh
 pip install -q nose
 nosetests -v
+
+# Validate example in README
+./scripts/validate-readme-example.sh
---
+++ scripts/validate-readme-example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tempFile=$(mktemp -t validate-readme)
+sed -n '/^```python/,/^```/ p' < README.md | sed '/^```/ d' > $tempFile
+
+# Validate syntax by compilation
+PYTHONPATH=. python3 -m py_compile $tempFile
+
+if [[ $? -eq 0 ]]
+then
+    echo "Example Python in README.md is syntactically valid"
+else
+    echo "Example Python in README.md is not syntactically valid!"
+fi
+
+exit $?
