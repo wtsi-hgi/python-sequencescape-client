c9644efe63e08f3adfe461179b6e85b643d2e178
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-06-06 14:30:54 +0100

    Fixed test cleanup issue.

--- sequencescape/tests/sqlalchemy/stub_database.py
+++ sequencescape/tests/sqlalchemy/stub_database.py
@@ -1,20 +1,27 @@
+import os
 import tempfile
+from typing import Tuple
 
+import atexit
 from sqlalchemy import create_engine
 
 from sequencescape._sqlalchemy._models import SQLAlchemyModel
 
 
-def create_stub_database():
+def create_stub_database() -> Tuple[str, str]:
     """
     Creates a stub database with a number of tables found in a Sequencescape database.
 
     An in-memory database is not used because sqlite in-memory databases are destroyed upon close.
-    :return: stub database
+    :return: stub database and the dialect
     """
-    file_handle, database_location = tempfile.mkstemp()
-    dialect = "sqlite"
+    def clean_up():
+        os.remove(database_location)
+    atexit.register(clean_up)
 
+    file_handle, database_location = tempfile.mkstemp()
+
+    dialect = "sqlite"
     engine = create_engine("sqlite:///%s" % database_location)
     SQLAlchemyModel.metadata.create_all(bind=engine)
 
